a25130399bba25808cca8cdd00b065f7931124b6
Jeremy Walton <jthelegoman@gmail.com>
2015-07-16 15:53:23 -0400

    Add endpoint for slack slash kudo command

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,6 +1,29 @@
 class KudosController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     @kudos = Kudo.all
+  end
+
+  def create
+    author = User.find_or_create_by(name: params[:user_name])
+    recipient = User.find_or_create_by(name: recipient_name)
+    kudo = Kudo.create(author_id: author.id, recipient_id: recipient.id, reason: kudo_reason)
+
+    render status: :ok, json: 'Good'
+  end
+
+  private
+
+  def parsed_kudo
+    /\A\/kudos to @(?<recipient>\w+) for (?<reason>.+)\z/.match(params[:command])
+  end
+
+  def recipient_name
+    parsed_kudo[:recipient]
+  end
+
+  def kudo_reason
+    parsed_kudo[:reason]
   end
 end
