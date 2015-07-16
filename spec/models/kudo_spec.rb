b0ceab40287b26e3f32900f7a7b038923cc6bac6
Jeremy Walton <jthelegoman@gmail.com>
2015-07-16 15:39:11 -0400

    Initialize and add rspec tests

--- spec/models/kudo_spec.rb
+++ spec/models/kudo_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Kudo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { User.create(name: 'You') }
+  let(:recipient) { User.create(name: 'Monster') }
+  let(:kudo) { Kudo.create(author_id: author.id, recipient_id: recipient.id, reason: 'For science') }
+
+  it "creates kudo with appropriate associations" do
+    expect(kudo.author).to eq author
+    expect(kudo.recipient).to eq recipient
+    expect(kudo.reason).to eq 'For science'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { User.create(name: 'You') }
+  let(:recipient) { User.create(name: 'Monster') }
+  let!(:kudo) { Kudo.create(author_id: author.id, recipient_id: recipient.id, reason: 'For science') }
+
+  it "creates user with appropriate associations" do
+    expect(author.given_kudos.first).to eq kudo
+    expect(recipient.received_kudos.first).to eq kudo
+  end
 end
